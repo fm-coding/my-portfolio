41170440c0b9b98e5fa00313ae85e6bcfb4ab171
fm-coding <133166438+fm-coding@users.noreply.github.com>
2025-04-14 14:59:17 +0300

    trying to deploy via github

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true, // Automatically opens browser on startup
+    open: true,
   },
+  base:"/faisal-portfolio",
 });
