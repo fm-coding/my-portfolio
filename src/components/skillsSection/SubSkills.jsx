92acade82b52b1d0ce6f8d855a86b14130e8db6c
fm-coding <133166438+fm-coding@users.noreply.github.com>
2025-04-15 11:23:33 +0300

    Fix screen width

--- src/components/skillsSection/SubSkills.jsx
+++ src/components/skillsSection/SubSkills.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const SubSkills = () => {
-    return (
-      <div className="border-y-2 border-lightGrey relative ">
-        <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full"></div>
-        <img
-          src="../../public/images/subSkills.jpg"
-          alt="subSkills image"
-          className=""
-        />
-      </div>
-    );
-  };
+  return (
+    <div className="border-y-2 border-lightGrey relative">
+      <div className="absolute bg-gradient-to-r from-orange to-cyan opacity-50 w-full h-full z-0"></div>
+      <img
+        src={`${import.meta.env.BASE_URL}images/subSkills.jpg`}
+        className="relative z-10 w-full h-auto"
+      />
+    </div>
+  );
+};
 
-export default SubSkills
+export default SubSkills;
